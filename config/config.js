ddc578e3e232fa3b098137ac019e7ffc9614e065
adhikariraju <raju@bytesnchips.com>
2018-07-25 11:31:06 +0545

    added token

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports={
-  WX_TOKEN:"MYWXTOKEN",
+  WX_TOKEN:"eu5md2v4PMuqYlWvQDbYCEVmEr1ElovI",
+  AESKEY:"TwMqwCDywCImxy0fWsssKAhPv2h9NZLUBV5f0oVdD8u",
   APPID: "wxc47d0eabcf02fe51",
   APPSECRET: "34183daf5a527c08fc3bbbddda05f3ac",
   db: "",
