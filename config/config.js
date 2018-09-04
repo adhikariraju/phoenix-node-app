e297064666d060928ecacf5606f43451e009e759
raju <raju@crossovernepal.com>
2018-09-04 21:05:10 +0800

    a

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ module.exports={
   AESKEY:"TwMqwCDywCImxy0fWsssKAhPv2h9NZLUBV5f0oVdD8u",
   APPID: "wxc47d0eabcf02fe51",
   APPSECRET: "34183daf5a527c08fc3bbbddda05f3ac",
-  db: "mongodb://adhikariraju5:pheonix123@ds121332.mlab.com:21332/heroku_sjncjzqg",
-  db_dev: 'mongodb://adhikariraju5:pheonix123@ds121332.mlab.com:21332/heroku_sjncjzqg',
+  db: "mongodb://127.0.0.1:27017/phoneix",
+  db_dev: 'mongodb://127.0.0.1:27017/phoneix',
 }
